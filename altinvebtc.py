13c625660b59e56611e1ef23c572bc2872a3ab4c
Koray Ademoglu <korayademoglu@gmail.com>
2022-04-09 23:57:44 +0300

    Altın alış ve BTCyi ara

---
+++ altinvebtc.py
@@ -0,0 +1,37 @@
+from lib2to3.pgen2 import driver
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+sayac = 1
+
+while True:
+    browser = webdriver.Chrome()
+
+    browser.implicitly_wait(3)
+
+    browser.get("https://tr.tradingview.com/symbols/FX_IDC-XAUTRYG/")
+
+    time.sleep(2)
+    alis = browser.find_element_by_xpath('//*[@id="anchor-page-1"]/div/div[3]/div[1]/div/div/div/div[1]/div[1]/span')
+
+    print(f"Alış Fiyatı: {alis.text}")
+
+
+    f = open("altın.txt", "a", encoding="utf-8")
+    f.write(f"Alış Fiyatı: {alis.text} \n")
+    f.close()
+    buton = browser.find_element_by_xpath('/html/body/div[2]/div[3]/div[2]/div[2]/div/div/button[1]')
+    buton.click()
+    aramaClick = browser.find_element_by_xpath('//*[@id="overlap-manager-root"]/div/div/div[2]/div/div/div[1]/div/div[1]/span/form/input')
+    aramaClick.send_keys("BTC")
+    time.sleep(1)
+    aramaClick.send_keys(Keys.ENTER)
+    time.sleep(1)
+    
+    if sayac==1:
+        print("döngü bitti")
+        break
+    else:
+        sayac+=1
